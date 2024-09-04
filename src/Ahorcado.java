990e172df39bf303f3a1c184b59220713e98ec15
VictorDanielRuiz <vdrp.041291@gmail.com>
2024-09-03 21:59:27 -0600

    Realizamos un juego de ahorcado en Java

---
+++ src/Ahorcado.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) throws Exception {
+        
+        // Clase scanner que nos permite que el usuario escriba
+        Scanner scanner = new Scanner(system.in);
+
+        // Declaraciones y asignaciones
+        String palabraSecreta = "inteligencia";
+        int intentosMaximos = 10;
+        int intentos = 0;
+        boolean palabraAdivinada = false;
+
+        //Arreglos:
+        char [] letrasAdivinadas = new char[palabraSecreta.length()];
+
+        // Estructura de control: Iterativa (Bucle)
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+            System.out.print(letrasAdivinadas[i]);
+        }
+
+        // Estructura de control: Iterativa (Bucle)
+
+        while (!palabraAdivinada && intentos) {
+            
+        }
+
+    }
+}
